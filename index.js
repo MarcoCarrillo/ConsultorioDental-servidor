76f02f016b2e7c5c87d0b0e1030b940ed2cb2b84
Marco <marco.carrillo.is@unipolidgo.edu.mx>
2021-10-15 16:56:38 -0500

    Creacion del server de express

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('desde index');
+const express = require('express');
+
+//Crear server 
+const app = express();
+
+//Puerto
+const PORT = process.env.PORT || 4000;
+
+//Pagina principal
+app.get('/', (req, res) =>{
+    res.send('Hola mundo')
+})
+
+//Arrancar server
+app.listen( PORT, ()=>{
+    console.log(`El servidor esta funcionando en el puerto ${PORT}`);
+});
+
