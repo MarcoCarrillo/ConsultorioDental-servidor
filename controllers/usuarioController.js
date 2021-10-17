e4589362bb042cfba5fd32600bf0d99fec046b79
Marco <marco.carrillo.is@unipolidgo.edu.mx>
2021-10-16 19:30:51 -0500

    Insertando nuevos usuarios en la DB

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,5 +1,18 @@
+const Usuario = require('../models/Usuario')
 
+exports.crearUsuario = async (req, res) => {
+    try {
+        let usuario;
+        // crea el nuevo usuario
+        usuario = new Usuario(req.body);
 
-exports.crearUsuario = (req, res) => {
-    console.log(req.body);
+        //Guardar usuario
+        await usuario.save();
+
+        //Confirmacion
+        res.send('Usuario creado correctamente');
+    } catch (error) {
+        console.log(error);
+        res.status(400).send('Hubo un error')
+    }
 }
