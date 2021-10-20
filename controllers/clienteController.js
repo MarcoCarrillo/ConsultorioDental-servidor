50e6004748afcefabb65c4790bc0a379dae24efc
Marco <marco.carrillo.is@unipolidgo.edu.mx>
2021-10-20 00:11:09 -0500

    Creacion basica de clientes
    Falta agregar validaciones y agregar el objectId del usuario al que pertenece este paciente para poder ligarlo

--- controllers/clienteController.js
+++ controllers/clienteController.js
@@ -1,4 +1,15 @@
+const Cliente = require('../models/Cliente');
 
 exports.crearCliente = async (req, res) => {
-    
+
+    try {
+        //Crear un nuevo cliente
+        const cliente = new Cliente(req.body);
+        cliente.save();
+        res.json(cliente);
+        console.log(cliente);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
 }
