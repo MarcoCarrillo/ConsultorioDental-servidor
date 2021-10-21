4dbf1e2da9dfed65ded61d0d3d05dcefb1a132f1
Marco <marco.carrillo.is@unipolidgo.edu.mx>
2021-10-20 19:01:21 -0500

    Usuario autenticado para crear nuevos pacientes
    Se verifica que el usuario que quiera agregar un nuevo paciente esté autenticado via jwt y ya autenticado pueda crear nuevos pacientes

--- controllers/clienteController.js
+++ controllers/clienteController.js
@@ -3,8 +3,13 @@ const Cliente = require('../models/Cliente');
 exports.crearCliente = async (req, res) => {
 
     try {
-        //Crear un nuevo cliente
+        //Crear un nuevo paciente
         const cliente = new Cliente(req.body);
+
+        //Guardar el creador via jsonwebtoken
+        cliente.creador = req.user.id;
+        console.log(req.user);
+
         cliente.save();
         res.json(cliente);
         console.log(cliente);
--- routes/clientes.js
+++ routes/clientes.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const clienteController = require('../controllers/clienteController');
+const auth = require('../middleware/auth');
 
 //Creacion de pacientes
 // api/clientes
 router.post('/',
+    auth,
+    clienteController.crearCliente
+);
+
+router.get('/',
+    auth,
     clienteController.crearCliente
 )
+
 
 module.exports = router;
 
