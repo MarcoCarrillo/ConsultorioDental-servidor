922b4b6be5ec8a0ea83e977aea00650001a33758
Marco <marco.carrillo.is@unipolidgo.edu.mx>
2021-10-31 23:04:37 -0600

    Insertando pagos al cliente

--- controllers/pagoController.js
+++ controllers/pagoController.js
@@ -4,9 +4,34 @@ const {validationResult} = require('express-validator');
 
 //Crear un nuevo pago
 exports.crearPago = async (req, res) =>{
+   
     //Revisar si hay errores con express validator
     const errores = validationResult(req);
     if( !errores.isEmpty() ){
         res.status(400).json({errores: errores.array()});
+    }
+
+    try {
+        //Extraer el cliente y comprobar si existe
+        const {cliente} = req.body;
+
+        const existeCliente = await Cliente.findById(cliente);
+        if(!existeCliente) {
+            return res.status(404).json({ msg: 'Cliente no encontrado' })
+        }
+
+        //Revisar si el cliente pertenece al usuario autenticado
+        if(existeCliente.creador.toString() !== req.user.id) {
+            return res.status(401).send({msg: 'No autorizado'})
+        }
+
+        //Crear el pago
+        const pago = new Pago(req.body);
+        await pago.save();
+        res.json(pago); 
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error')
     }
 }
