d819f8c20fcf975bd6942e46666b50a08d0dcdd8
Marco <marco.carrillo.is@unipolidgo.edu.mx>
2021-11-03 14:15:55 -0600

    Se elimina validacion que ya está en el frontend

--- routes/auth.js
+++ routes/auth.js
@@ -8,10 +8,6 @@ const auth = require('../middleware/auth');
 //Iniciar sesion
 // api/auth
 router.post('/', 
-    [
-        check('usuario', 'El usuario es obligatorio').not().isEmpty(),
-        check('password', 'El password debe ser minimo de 6 caracteres').isLength({min: 6})
-    ],
     authController.autenticarUsuario
 );
 
