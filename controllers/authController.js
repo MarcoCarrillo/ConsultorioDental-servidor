3f75950bfbe419e418d2c689da3f635bf9936e90
Marco <marco.carrillo.is@unipolidgo.edu.mx>
2021-11-03 13:49:50 -0600

    Enviando el objeto del usuario autenticado menos la contraseña

--- controllers/authController.js
+++ controllers/authController.js
@@ -51,7 +51,7 @@ exports.autenticarUsuario = async (req, res) => {
 //Obtiene el usuario autenticado
 exports.usuarioAutenticado = async (req, res) => {
     try {
-        const usuario = await Usuario.findById(req.user.id);
+        const usuario = await Usuario.findById(req.user.id).select('-password');
         res.json({usuario});
     } catch (error) {
         console.log(error);
